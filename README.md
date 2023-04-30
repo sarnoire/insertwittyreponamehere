e4e939b560b1a151897bb4875b860667404edbe2
sndesantis <sarahndesantis@gmail.com>
2023-04-30 11:00:01 -0600

    Pushing to Heroku

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # insertwittyreponamehere
 Module 19 Challenge: Progressive Web Applications (PWA) Challenge: Text Editor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++ https://developer.chrome.com/docs/workbox/reference/#registerRoute
++ https://developer.chrome.com/docs/workbox/reference/
++ https://webpack.js.org/concepts/
++ https://webpack.js.org/plugins/
++ https://webpack.js.org/loaders/css-loader/
++ https://babeljs.io/docs/
++ https://web.dev/progressive-web-apps/
++ https://coding-boot-camp.github.io/full-stack/heroku/heroku-deployment-guide
---
+++ main.js
@@ -0,0 +1,24 @@
+import { openDB } from 'idb';
+
+const dbPromise = openDB('notes-db', 1, {
+  upgrade(db) {
+    db.createObjectStore('notes', { keyPath: 'id', autoIncrement: true });
+  },
+});
+
+function addNoteToDB(note) {
+    dbPromise.then((db) => {
+      const tx = db.transaction('notes', 'readwrite');
+      const store = tx.objectStore('notes');
+      store.put(note);
+      return tx.complete;
+    });
+  }
+  
+async function getAllNotesFromDB() {
+    const db = await dbPromise;
+    const tx = db.transaction('notes', 'readonly');
+    const store = tx.objectStore('notes');
+    return await store.getAll();
+  }
+  
